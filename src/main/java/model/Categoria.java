a25dd2e64279b7fb3142fd6bcbeaab02f6345b6d
Leonardo <Leonardo@LEO>
2025-05-21 20:06:25 -0300

    Criação da classe categoria

--- src/main/java/model/Categoria.java
+++ src/main/java/model/Categoria.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author dudu
- */
 public class Categoria {
-    
+
+    private int idCategoria;
+    private String nome;
+
+    public Categoria() {
+        this(0,"");
+    }
+
+    public Categoria(int idCategoria, String nome) {
+        this.idCategoria = idCategoria;
+        this.nome = nome;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
