40cbe166950a0ee4c09918322490d6b123350782
rafae <rafae@pc>
2025-05-23 15:01:37 -0300

    Adicionei a String "email" ao cliente

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -3,15 +3,17 @@ package model;
 public class Cliente {
     
     private String nome;
+    private String email;
     private String cpf;
     private int idCliente;
     
     public Cliente(){
-        this("","", 0);
+        this("","","", 0);
     }
     
-    public Cliente(String nome, String cpf, int idCliente) {
+    public Cliente(String nome, String email, String cpf, int idCliente) {
         this.nome = nome;
+        this.email = email;
         this.cpf = cpf;
         this.idCliente = idCliente;
     }
@@ -22,6 +24,15 @@ public class Cliente {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getCpf() {
