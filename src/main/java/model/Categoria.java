65aaaae5a42a26034bd93322c17daa5dae96f80b
leoclasen <leonardolclasen@gmail.com>
2025-05-27 18:23:17 -0300

    refactor: mudança nos atributos da classe categoria.

--- src/main/java/model/Categoria.java
+++ src/main/java/model/Categoria.java
@@ -4,14 +4,16 @@ public class Categoria {
 
     private int idCategoria;
     private String nome;
+    private String tipo;
 
     public Categoria() {
-        this(0,"");
+        this(0, "", "");
     }
 
-    public Categoria(int idCategoria, String nome) {
+    public Categoria(int idCategoria, String nome, String tipo) {
         this.idCategoria = idCategoria;
         this.nome = nome;
+        this.tipo = tipo;
     }
 
     public int getIdCategoria() {
@@ -28,5 +30,13 @@ public class Categoria {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 }
