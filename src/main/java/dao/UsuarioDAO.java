36c5d3803c064101f55f25dcd084102617db6e18
dudunm <dudunienkotterm@gmail.com>
2025-06-01 21:19:07 -0300

    Imports criados na classe UsuarioDAO - Eduardo N Marcelino

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author dudun
- */
+import model.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UsuarioDAO {
+   
+    
     
 }
